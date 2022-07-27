8b3813dec4ebb2ba7e47c6edcd9cf577369dcac0
koldin108902 <alex108902@naver.com>
2022-07-28 00:19:52 +0900

    comments of Autowired

--- hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -13,6 +13,10 @@ public class MemberService {
     private final MemberRepo memberRepo;
 
 //    memberRepo를 초기화하기 위한 생성자.
+//    Autowired는 spring bean에 등록을 자동으로 해주는 어노테이션
+//    Spring bean은 객체인데 Spring IoC에서 관리하는 객체임.
+//    @Container가 포함된 클래스는 자동으로 Spring bean에 등록됨.
+//    아래와 같이 생성자에 클래스명을 붙이면 해당 이름의 bean에 있는 객체를 반환해줌.
     @Autowired
     public MemberService(MemberRepo memberRepo) {
         this.memberRepo = memberRepo;
