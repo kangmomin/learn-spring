f65317719a9ba9d630ce45bf184551400fa30014
koldin108902 <alex108902@naver.com>
2022-07-28 10:43:43 +0900

    자바 코드로 Spring bean등록할때의 이점

--- hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -12,15 +12,29 @@ import java.util.Optional;
 public class MemberService {
     private final MemberRepo memberRepo;
 
+//    @Autowired private MemberRepo memberRepo;
+//    field주입 선언 당시를 제외하곤 값의 변경이 불가하기에 비추.
+
 //    memberRepo를 초기화하기 위한 생성자.
 //    Autowired는 spring bean에 등록을 자동으로 해주는 어노테이션
 //    Spring bean은 객체인데 Spring IoC에서 관리하는 객체임.
 //    @Container가 포함된 클래스는 자동으로 Spring bean에 등록됨.
 //    아래와 같이 생성자에 클래스명을 붙이면 해당 이름의 bean에 있는 객체를 반환해줌.
+
+//    생성자 주입
+//    생성 시점에만 변경후 변경을 하지 않기 때문에 안전하다.
+//    변수에 final이 붙을 수 있는 이유는 생성자에서 성정하기 때문인 것 같다.
     @Autowired
     public MemberService(MemberRepo memberRepo) {
         this.memberRepo = memberRepo;
     }
+
+//    setter 주입
+//    퍼블릭이라 아무 개발자가 접근이 가능해서 요즘은 거의 사용치 않음.
+//    @Autowired
+//    public void setMemberRepo(MemberRepo memberRepo) {
+//        this.memberRepo = memberRepo;
+//    }
 
 //    sign up
     public Long join(Member member) {
--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -6,6 +6,7 @@ import hello.hellospring.repository.MemoryMemberRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//추후 MemberRepo의 생성자 코드만 살짝 바꾸면 다른 코드 수정 없이 DB를 변경하는 등의 작업이 가능함.
 @Configuration
 public class SpringConfig {
     @Bean
