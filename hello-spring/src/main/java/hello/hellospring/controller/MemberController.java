5f6cd794b2d8923fdb08643d821245b385ccc388
koldin108902 <alex108902@naver.com>
2022-08-03 03:15:43 +0900

    회원 웹 기능 - 홈 화면 추가

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,7 +4,6 @@ import hello.hellospring.Service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
@@ -15,5 +14,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String crewateForm() {
+        return "members/createMemberForm";
     }
 }
