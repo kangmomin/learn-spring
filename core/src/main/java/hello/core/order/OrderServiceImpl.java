c28382ef3fcee88837c53c3e4351785c533f33a5
koldin108902 <alex108902@naver.com>
2022-08-29 23:15:47 +0900

    update: qualifier의 다수의 조회 빈

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,21 +3,23 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepo;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final private 변수를 생성자로 만들어줌.(Lombok)
+//@RequiredArgsConstructor // final private 변수를 생성자로 만들어줌.(Lombok)
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepo memberRepo;
     private final DiscountPolicy discountPolicy;
 
     // DiscountPolicy가 두개일 시 필드 명, 파라티머 명과 같은 결곽 값을 가져온다.
-//    public OrderServiceImpl(MemberRepo memberRepo, DiscountPolicy rateDiscountPolicy) {
-//        this.memberRepo = memberRepo;
-//        this.discountPolicy = rateDiscountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepo memberRepo, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+        this.memberRepo = memberRepo;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
