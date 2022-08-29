c074be0b9f99d19db1fe99db0839c460fd34c541
koldin108902 <alex108902@naver.com>
2022-08-29 23:09:34 +0900

    fixed: 조회 빈이 2개 이상일 상황 테스트

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -12,7 +12,7 @@ import static org.springframework.context.annotation.ComponentScan.*;
 @Configuration
 @ComponentScan (
         // default는 해당 config를 선언한 패키지를 시작으로 하위로 내려감.
-        basePackages = "hello.core.member", // 컴포넌트 스캔의 시작위치를 설정함.
+        basePackages = "hello.core", // 컴포넌트 스캔의 시작위치를 설정함.
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
