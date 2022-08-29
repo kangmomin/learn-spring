d95f3b6d508d85a856d3d8872ce3257557e93c7f
koldin108902 <alex108902@naver.com>
2022-08-29 23:13:07 +0900

    update: autowired의 다수의 조회 빈

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,13 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepo memberRepo;
     private final DiscountPolicy discountPolicy;
-    
+
+    // DiscountPolicy가 두개일 시 필드 명, 파라티머 명과 같은 결곽 값을 가져온다.
+//    public OrderServiceImpl(MemberRepo memberRepo, DiscountPolicy rateDiscountPolicy) {
+//        this.memberRepo = memberRepo;
+//        this.discountPolicy = rateDiscountPolicy;
+//    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepo.findById(memberId);
