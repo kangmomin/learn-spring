ebed11413d93c66dc0a7dda67ed5d5f1faa8f941
koldin108902 <alex108902@naver.com>
2022-09-01 11:41:58 +0900

    add: lombok check

--- jpashop/src/main/java/com/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/com/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("hello");
+		System.out.println(hello.getData());
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
