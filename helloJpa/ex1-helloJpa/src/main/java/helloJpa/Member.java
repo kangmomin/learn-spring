1a2dd239d85c3357315fc332b0a2b4c116677106
koldin108902 <alex108902@naver.com>
2022-09-05 22:56:06 +0900

    update: 필드, 컬럼 매핑

--- helloJpa/ex1-helloJpa/src/main/java/helloJpa/Member.java
+++ helloJpa/ex1-helloJpa/src/main/java/helloJpa/Member.java
@@ -1,34 +1,39 @@
 package helloJpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.sun.istack.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
-    @Id
+    @Id @Column(nullable = false)
     private Long id;
-    private String name;
 
-    public Member() {}
+    @Column(name = "name")
+    private String username;
+    private Integer age;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Enumerated(EnumType.STRING) // EnumType.ORDINAL == Enum순서 저장, String == 이름 그대로 저장 << String 추천
+    private RoleType roleType;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 
-    public Long getId() {
-        return id;
-    }
+    // 둘다 date지만 최근 아래의 타입은 하이버네이트에서도 지원해줌.
+    private LocalDateTime testLocalData;
 
-    public String getName() {
-        return name;
+    @Lob
+    private String description;
+
+    @Transient // 매핑 하지 않음.
+    private String onlyMemory;
+
+
+    public Member() {
     }
 }
