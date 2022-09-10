d16615805e4c83dc616cfb5cd58d509fe52f1e7b
koldin108902 <alex108902@naver.com>
2022-09-10 19:13:22 +0900

    add: 실전예제4 상속관계 매핑

---
+++ helloJpa/jpaBook/src/main/java/jpabook/jpashop/domain/Book.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Book extends Item {
+
+    private String author;
+    private String isbm;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbm() {
+        return isbm;
+    }
+
+    public void setIsbm(String isbm) {
+        this.isbm = isbm;
+    }
+}
--- helloJpa/jpaBook/src/main/java/jpabook/jpashop/domain/Item.java
+++ helloJpa/jpaBook/src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
+public abstract class Item extends BaseEntity {
     @Id @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "item_id")
     private Long id;
 
     private String name;
