28286ed80305b7a44be4ad3b169301be15300300
koldin108902 <alex108902@naver.com>
2022-09-13 20:08:51 +0900

    add: 고아 객체 처리

--- helloJpa/ex1-helloJpa/src/main/java/helloJpa/Parent.java
+++ helloJpa/ex1-helloJpa/src/main/java/helloJpa/Parent.java
@@ -15,10 +15,20 @@ public class Parent {
 
     private String name;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL) // 영속성 전이.
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true) // 영속성 전이.
     // 데이터는 영속성 컨텍스트에 저장되는데 이 때 데이터들을 따로 저장하기보단
     // 가장 상귀 객체에 전이 함으로써 한번에 저장될 수 있도록 한다
+
+    // orphanRemoval == 부모 요소가 사라질 때 자식요소도 함께 지워줌(O T M, O T O 에서만 사용)
     private List<Child> childList = new ArrayList<>();
+
+    public List<Child> getChildList() {
+        return childList;
+    }
+
+    public void setChildList(List<Child> childList) {
+        this.childList = childList;
+    }
 
     public void addChild(Child child) {
         childList.add(child);
