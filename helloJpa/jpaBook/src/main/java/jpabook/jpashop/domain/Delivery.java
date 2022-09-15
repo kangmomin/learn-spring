206f3ebe35a048627167835093619d60d04c75d3
koldin108902 <alex108902@naver.com>
2022-09-15 12:07:21 +0900

    add: 값 타입 매핑

--- helloJpa/jpaBook/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ helloJpa/jpaBook/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -8,12 +8,43 @@ public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "delivery_id")
     private Long id;
-
-    private String city;
-    private String street;
-    private String zipcode;
     private DeliveryStatus status;
+
+    @Embedded
+    private Address address;
 
     @OneToOne(fetch = FetchType.LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DeliveryStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DeliveryStatus status) {
+        this.status = status;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
