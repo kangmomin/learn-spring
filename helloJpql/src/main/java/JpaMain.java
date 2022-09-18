c0cf52b085359b1e51e09308e3e5b59fa8fa2981
koldin108902 <alex108902@naver.com>
2022-09-18 22:22:01 +0900

    update: 경로 표현식

--- helloJpql/src/main/java/JpaMain.java
+++ helloJpql/src/main/java/JpaMain.java
@@ -34,7 +34,12 @@ public class JpaMain {
 //                    "from Member m";
 //            String query = "select coalesce(m.name, '이름 없는 회원') from Member m"; // 값이 없다면
 //            String queryNullif = "select nullif(m.name, '관리자') from Member m"; // 관리자라면 null로
-            String query = "select function('group_concat', m.name) From Member m "; // custom function
+            // m.name과 같은 경로 탐색이 더 이상 불가능한 상태를 상태필드라 함.
+
+            // 묵시적 조인이 발생하는 갑들. 걍 쓰지 마삼.
+            // m.team과 같이 묵시적 조인이 발생하고 그 하위로 더 많은 값을 읽을 수 있는 것을 단일 값 연관 경로라고 함.
+            // 컬랙션 값의 경우 size를 제외한 값을 탐색할 수 없음
+            String query = "select m.name From Member m "; // custom function
             List<String> resultList = em.createQuery(query, String.class).getResultList();
             for (String s : resultList) {
                 System.out.println("s = " + s);
