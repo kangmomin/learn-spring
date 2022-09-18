93135a0af6bbb6cc0b76b8e2998efe66600be7eb
koldin108902 <alex108902@naver.com>
2022-09-18 13:55:05 +0900

    update: 커스텀 함수

--- helloJpql/src/main/java/JpaMain.java
+++ helloJpql/src/main/java/JpaMain.java
@@ -1,4 +1,5 @@
-package jpql;
+import jpql.Member;
+import jpql.Team;
 
 import javax.persistence.*;
 import java.util.List;
@@ -31,8 +32,9 @@ public class JpaMain {
 //                    "   when m.age >= 60 then '경로요금'" +
 //                    "   else '일반요금' END " +
 //                    "from Member m";
-            String query = "select coalesce(m.name, '이름 없는 회원') from Member m"; // 값이 없다면
-            String queryNullif = "select nullif(m.name, '관리자') from Member m"; // 관리자라면 null로
+//            String query = "select coalesce(m.name, '이름 없는 회원') from Member m"; // 값이 없다면
+//            String queryNullif = "select nullif(m.name, '관리자') from Member m"; // 관리자라면 null로
+            String query = "select function('group_concat', m.name) From Member m "; // custom function
             List<String> resultList = em.createQuery(query, String.class).getResultList();
             for (String s : resultList) {
                 System.out.println("s = " + s);
