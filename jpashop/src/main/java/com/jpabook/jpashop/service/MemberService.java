27380d737e95291b831d657842bd7b3bb97367b7
koldin108902 <alex108902@naver.com>
2022-10-03 22:05:31 +0900

    add: 회원 등록

--- jpashop/src/main/java/com/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/com/jpabook/jpashop/service/MemberService.java
@@ -24,7 +24,7 @@ public class MemberService {
 
     // 회원 가입
     @Transactional
-    public Long  join(Member member) {
+    public Long join(Member member) {
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
