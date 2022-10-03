36b59d88eb86217eab429fd698292006c7210916
koldin108902 <alex108902@naver.com>
2022-10-03 23:57:51 +0900

    add: 주문 목록 및 취소

--- jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashop.domain;
 
+import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- jpashop/src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -8,9 +8,12 @@ import com.jpabook.jpashop.domain.item.Item;
 import com.jpabook.jpashop.repository.ItemRepository;
 import com.jpabook.jpashop.repository.MemberRepository;
 import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -43,7 +46,7 @@ public class OrderService {
         order.cancel();
     }
 
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll();
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 }
