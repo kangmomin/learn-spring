1ee1b29e2ecb76f9e212f23c0a18cd98685e9adf
koldin108902 <alex108902@naver.com>
2022-10-03 22:11:44 +0900

    add: 회원 목록

--- jpashop/src/main/java/com/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/com/jpabook/jpashop/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.jpabook.jpashop.controller;
 import com.jpabook.jpashop.domain.Address;
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,5 +43,15 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+
+        System.out.println("members = " + members);
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
