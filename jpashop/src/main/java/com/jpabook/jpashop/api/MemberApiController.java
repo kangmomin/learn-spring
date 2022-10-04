b01dadbd1f8688a76e9405d3975ee94de46f8319
koldin108902 <alex108902@naver.com>
2022-10-04 10:22:24 +0900

    add: 회원 등록 api

--- jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -1,2 +1,51 @@
-package com.jpabook.jpashop.api;public class MemberApiController {
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    // @RequestBody == body를 member에 넣어줌.
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long memberId = memberService.join(member);
+
+        return new CreateMemberResponse(memberId);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberv2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+
+        member.setName(request.name);
+        Long id = memberService.join(member);
+
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
 }
