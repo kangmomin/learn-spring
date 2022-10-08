6a9ccf50b79ea361902379d43dabd2dfe73c7f6c
koldin108902 <alex108902@naver.com>
2022-10-08 23:50:32 +0900

    update: 주문 조회 V1 - 엔티티 직접 노출

---
+++ jpashop/src/main/java/com/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,39 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public Result<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream()
+                    .forEach(o -> o.getItem().getName());
+        }
+        return new Result(all);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+}
