6130bf831e64ef8e7549e7dd61479a4e733abb31
koldin108902 <alex108902@naver.com>
2022-12-06 03:20:24 +0900

    update: spring data repository로 변경

--- queryDSL/src/main/java/study/queryDSL/repository/MemberRepository.java
+++ queryDSL/src/main/java/study/queryDSL/repository/MemberRepository.java
@@ -1,101 +1,13 @@
 package study.querydsl.repository;
 
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
-import study.querydsl.dto.MemberSearchCondition;
-import study.querydsl.dto.MemberTeamDto;
-import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
-import study.querydsl.entity.QMember;
-import study.querydsl.entity.QTeam;
-import study.querydsl.entity.Team;
 
 import java.util.List;
 
-import static study.querydsl.entity.QMember.*;
-import static study.querydsl.entity.QTeam.*;
-
 @Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
-    private final JPAQueryFactory query;
-
-    public List<Member> findAll() {
-        return query
-                .selectFrom(member)
-                .fetch();
-    }
-
-    public List<Member> findByUsername(String username) {
-        return query
-                .selectFrom(member)
-                .where(member.username.eq(username))
-                .fetch();
-    }
-
-    public List<MemberTeamDto> searchBuilder(MemberSearchCondition condition) {
-        BooleanBuilder builder = new BooleanBuilder();
-        if (StringUtils.hasText(condition.getUsername())) {
-            builder.and(member.username.eq(condition.getUsername()));
-        }
-        if (StringUtils.hasText(condition.getTeamName())) {
-            builder.and(member.team.name.eq(condition.getTeamName()));
-        }
-        if (condition.getAgeGoe() != null) {
-            builder.and(member.age.goe(condition.getAgeGoe()));
-        }
-        if (condition.getAgeLoe() != null) {
-            builder.and(member.age.loe(condition.getAgeLoe()));
-        }
-
-        return query
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"),
-                        team.name.as("teamName")
-                ))
-                .from(member)
-                .leftJoin(member.team, team)
-                .where(builder)
-                .fetch();
-    }
-    public List<MemberTeamDto> searchWhere(MemberSearchCondition condition) {
-        return query
-                .select(new QMemberTeamDto(
-                        member.id.as("memberId"),
-                        member.username,
-                        member.age,
-                        team.id.as("teamId"),
-                        team.name.as("teamName")
-                ))
-                .from(member)
-                .leftJoin(member.team, team)
-                .where(
-                        usernameEq(condition.getUsername()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe())
-                )
-                .fetch();
-    }
-
-    private BooleanExpression usernameEq(String username) {
-        return StringUtils.hasText(username) ? member.username.eq(username) : null;
-    }
-    private BooleanExpression teamNameEq(String teamName) {
-        return StringUtils.hasText(teamName) ? member.team.name.eq(teamName) : null;
-    }
-    private BooleanExpression ageGoe(Integer goe) {
-        return goe != null ? member.age.goe(goe) : null;
-    }
-    private BooleanExpression ageLoe(Integer loe) {
-        return loe != null ? member.age.loe(loe) : null;
-    }
+    List<Member> findByUsername(String username);
 }
