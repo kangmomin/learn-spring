39aa06f6bc5b0ae4bc0768f7c5a72af4358d801e
koldin108902 <alex108902@naver.com>
2022-12-06 04:53:11 +0900

    fixed: 오타 수정

---
+++ queryDSL/src/main/java/study/queryDSL/controller/MemberController.java
@@ -0,0 +1,23 @@
+package study.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.repository.MemberRepository;
+import study.querydsl.repository.MemberRepositoryImpl;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberRepository.searchWhere(condition);
+    }
+}
